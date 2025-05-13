63665d2891616a3639be2f93af54fb1cc681d367
Nicolas Sosa (nss) <nico77@gmail.com>
2025-05-13 13:40:08 -0300

    insync

--- scripts_gui/install_common_apps.sh
+++ scripts_gui/install_common_apps.sh
@@ -4,6 +4,9 @@
 echo "Installing insync..."
 curl -L https://apt.insync.io/insynchq.gpg 2>/dev/null | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/insynchq.gpg 1>/dev/null
 # fix hardcoded ubuntu release name
-sudo echo "deb http://apt.insync.io/ubuntu plucky non-free contrib" /etc/apt/sources.list.d/insync.list
+sudo echo "deb http://apt.insync.io/ubuntu plucky non-free contrib" > /etc/apt/sources.list.d/insync.list
 sudo apt-get update
-sudo apt-get install insync
+sudo apt install python3-nautilus -y
+sudo apt-get install insync insync-nautilus -y
+
+
