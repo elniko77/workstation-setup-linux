6c169dde1b3fc706d5576b7527b092652ffeee98
Nicolas Sosa (nss) <nico77@gmail.com>
2025-05-13 13:26:01 -0300

    Config shell

--- scripts_ubuntu/configure_shell.sh
+++ scripts_ubuntu/configure_shell.sh
@@ -3,5 +3,13 @@
 
 echo "Installing starship..."
 curl -sS https://starship.rs/install.sh | sh
+eval "$(starship init bash)"
 starship preset pastel-powerline -o ~/.config/starship.toml
 
+echo "Lazyvim..."
+mv ~/.config/nvim{,.bak}
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
