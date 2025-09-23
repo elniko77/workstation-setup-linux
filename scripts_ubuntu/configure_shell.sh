d5566b84fbde624cdd53d38cd538c9a85eef8a6d
Nicolas Sosa (nss) <nico77@gmail.com>
2025-09-23 14:58:38 +0000

    tmux tpm and catppuccin

--- scripts_ubuntu/configure_shell.sh
+++ scripts_ubuntu/configure_shell.sh
@@ -6,10 +6,17 @@ curl -sS https://starship.rs/install.sh | sh
 eval "$(starship init bash)"
 starship preset pastel-powerline -o ~/.config/starship.toml
 
-echo "Lazyvim..."
+echo "Installing Lazyvim..."
 mv ~/.config/nvim{,.bak}
 mv ~/.local/share/nvim{,.bak}
 mv ~/.local/state/nvim{,.bak}
 mv ~/.cache/nvim{,.bak}
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
+
+echo "Installing tmux tpm plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Install catppuccin tmux themes"
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
